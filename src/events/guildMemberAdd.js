15e3f9fa6d178a92e37750e17acb5189b288c703
Altuğ <altugkisisel@gmail.com>
2026-03-05 21:32:16 +0300

    hotfix

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -3,7 +3,7 @@ const config = require("../../config.js");
 const db = require("quick.db");
 
 module.exports = {
-    name: 'guildMemberRemove',
+    name: 'guildMemberAdd',
     execute: async (member) => {
 
         try {
@@ -19,7 +19,6 @@ module.exports = {
                     ticketChannel.send({ embeds: [getEmbedInteraction(config.ticket.messages.ticketOwnerJoined.embed, { locale: ticketInfo.locale })], components: [getAfterTicketCloseEmbedButtonRow({ locale: ticketInfo.locale }, "reopen")] }).then(async (msg) => {
                         await db.push(`tickets.${ticketChannelId}.deletableMessages`, msg.id)
                     })
-                    console.log()
                 } else {
                     db.delete(`tickets.${ticketChannelId}`)
                 }
